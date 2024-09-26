ea382305c36a1f7c994da63b7cd393b847895d5e
Zerko3412 <99601584+Zerko3412@users.noreply.github.com>
2024-09-26 16:18:00 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+print('1st program')
+result1 = (9 ** 0.5) * 5
+print(result1)
+
+print('2st program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('3st program')
+result2 = (2 * 2 + 2)
+result3 = (2 * (2 + 2))
+print(result2)
+print(result3)
+print(result2 == result3)
+
+print('4st program')
+string_number = '123.456'
+float_number = float(string_number)
+shifted_number = float_number * 10
+int_number = int(shifted_number)
+first_digit_after_decimal = int_number % 10
+print(first_digit_after_decimal)
+
+
+
+
